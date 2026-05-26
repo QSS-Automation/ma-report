9f59769c95ff6f5c10eea05552300bb60f67d3b5
QSS-Automation <qss-automation@quandatics.com>
2026-05-26 16:28:02 +0800

    Update msalConfig.js

--- frontend/src/auth/msalConfig.js
+++ frontend/src/auth/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     clientId:    process.env.REACT_APP_MS_CLIENT_ID,
     authority:   `https://login.microsoftonline.com/${process.env.REACT_APP_MS_TENANT_ID}`,
     redirectUri: process.env.REACT_APP_REDIRECT_URI || "http://localhost:3000",
-    navigateToLoginRequestUrl: true,
+    navigateToLoginRequestUrl: false,
   },
   cache: {
     cacheLocation:          "sessionStorage",  // don't use localStorage
