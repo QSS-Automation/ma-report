412cb042212cb5bf876cf2c38a4c2e6168159d97
QSS-Automation <qss-automation@quandatics.com>
2026-06-03 11:37:15 +0800

    Delete frontend directory

--- frontend/src/components/Purchases/Purchases.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import InvoiceTab from "../Sales/InvoiceTab";
-export default function Purchases({ entity = "QM", setEntity, entities = [] }) {
-  return <InvoiceTab tab="pur" entity={entity} setEntity={setEntity} entities={entities} />;
-}
